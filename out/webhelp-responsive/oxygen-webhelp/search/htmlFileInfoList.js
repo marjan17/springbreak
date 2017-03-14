48347af1b15af9211368a15a10c3dc35842d4431
marjan17 <marjan17@vt.edu>
2017-03-14 10:15:37 -0400

    ts-troubleshoot

---
+++ out/webhelp-responsive/oxygen-webhelp/search/htmlFileInfoList.js
@@ -0,0 +1,7 @@
+fil = new Array();
+fil["0"]= "concepts/c-springbreak.html@@@Spring Break 2017@@@Maggie will be going home to Northern Virginia this Spring Break. It is her last Spring Break as an undergraduate and she wants to spend it at home...";
+fil["1"]= "tasks/t-money.html@@@How to Pay@@@Spring break isn&apos;t always cheap. It costs money to relax and have a good time, that&apos;s why it&apos;s important to consdier funding and spending...";
+fil["2"]= "tasks/t-sleep.html@@@Sweet Dreams@@@Here we will learn about the sleeping arrangments during the stay at NOVA...";
+fil["3"]= "tasks/t-springbreaktasks.html@@@Where To?@@@Here we will learn about where the spring break trip will take us...";
+fil["4"]= "tasks/t-time.html@@@Valuable Time@@@The break is just a week but Maggie wants to do a lot while she is at NOVA. She may go to her beach house with her family, she will probably work on some homework, go swimming,\n        eat home-cooked food and play with her dog...";
+fil["5"]= "tasks/t-transporation.html@@@Transporatation@@@Blacksburg and Chantily are very far away. The four hour travel will be long and tiring. Maggie wants to get there in the most efficient way possible...";
---
+++ out/webhelp-responsive/oxygen-webhelp/search/htmlFileList.js
@@ -0,0 +1,9 @@
+//List of files which are indexed.
+fl = new Array();
+fl["0"]= "concepts/c-springbreak.html";
+fl["1"]= "tasks/t-money.html";
+fl["2"]= "tasks/t-sleep.html";
+fl["3"]= "tasks/t-springbreaktasks.html";
+fl["4"]= "tasks/t-time.html";
+fl["5"]= "tasks/t-transporation.html";
+var doStem = false;searchLoaded = true;
